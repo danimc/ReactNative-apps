d724a75341141f2f63c39a472d561c591cfb0fdf
Daniel Mora <daniel_k310a@hotmail.com>
2024-12-04 18:22:30 -0600

    feat: integrate useCalculator hook to update formula and handle button presses in CalculatorApp

--- calculator/app/index.tsx
+++ calculator/app/index.tsx
@@ -4,12 +4,15 @@ import { globalStyles } from '@/styles/global-styles'
 import ThemeText from '@/components/ThemeText'
 import CalculatorButton from '@/components/CalculatorButton'
 import { Colors } from '@/constants/Colors'
+import { useCalculator } from '@/hooks/useCalculator'
 
 const CalculatorApp = () => {
+  const { formula, buildNumber } = useCalculator()
+
   return (
     <View style={globalStyles.calculatorContainer}>
       <View style={{ paddingHorizontal: 30, marginBottom: 20 }}>
-        <ThemeText variant="primary">50x50</ThemeText>
+        <ThemeText variant="primary">{formula}</ThemeText>
         <ThemeText variant="secondary">1000</ThemeText>
       </View>
       <View style={globalStyles.row}>
@@ -19,25 +22,29 @@ const CalculatorApp = () => {
         <CalculatorButton label="%" color={Colors.orange} />
       </View>
       <View style={globalStyles.row}>
-        <CalculatorButton label="7" />
-        <CalculatorButton label="8" />
-        <CalculatorButton label="9" />
+        <CalculatorButton onPress={() => buildNumber('7')} label="7" />
+        <CalculatorButton onPress={() => buildNumber('8')} label="8" />
+        <CalculatorButton onPress={() => buildNumber('9')} label="9" />
         <CalculatorButton label="X" color={Colors.orange} />
       </View>
       <View style={globalStyles.row}>
-        <CalculatorButton label="4" />
-        <CalculatorButton label="5" />
-        <CalculatorButton label="6" />
+        <CalculatorButton onPress={() => buildNumber('4')} label="4" />
+        <CalculatorButton onPress={() => buildNumber('5')} label="5" />
+        <CalculatorButton onPress={() => buildNumber('6')} label="6" />
         <CalculatorButton label="-" color={Colors.orange} />
       </View>
       <View style={globalStyles.row}>
-        <CalculatorButton label="1" />
-        <CalculatorButton label="2" />
-        <CalculatorButton label="3" />
+        <CalculatorButton onPress={() => buildNumber('1')} label="1" />
+        <CalculatorButton onPress={() => buildNumber('2')} label="2" />
+        <CalculatorButton onPress={() => buildNumber('3')} label="3" />
         <CalculatorButton label="+" color={Colors.orange} />
       </View>
       <View style={globalStyles.row}>
-        <CalculatorButton label="0" doubleSize />
+        <CalculatorButton
+          onPress={() => buildNumber('0')}
+          label="0"
+          doubleSize
+        />
         <CalculatorButton label="." />
         <CalculatorButton label="=" color={Colors.orange} />
       </View>
