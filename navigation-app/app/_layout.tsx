3a1b90c48aaeff67f8beea650a6d3844ab77b0d8
Daniel Mora <daniel_k310a@hotmail.com>
2025-02-22 15:43:20 -0600

    feat: add WorkSans font family support and implement font loading in layout

--- navigation-app/app/_layout.tsx
+++ navigation-app/app/_layout.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import { Slot } from 'expo-router'
-
+import React, { useEffect } from 'react'
+import { Slot, SplashScreen } from 'expo-router'
+import { useFonts } from 'expo-font'
 import './global.css'
 
+SplashScreen.preventAutoHideAsync()
+
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    'WorkSans-Black': require('../assets/fonts/WorkSans-Black.ttf'),
+    'WorkSans-Medium': require('../assets/fonts/WorkSans-Medium.ttf'),
+    'WorkSans-Light': require('../assets/fonts/WorkSans-Light.ttf'),
+  })
+
+  useEffect(() => {
+    if (error) throw error
+    if (fontsLoaded) SplashScreen.hideAsync()
+  }, [fontsLoaded, error])
+
+  if (!fontsLoaded && !error) return null
+
   return <Slot />
 }
 
