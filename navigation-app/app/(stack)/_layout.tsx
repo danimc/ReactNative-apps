b8d226ea55262553fe0ca2fc34f8f2ac0ac8ecaa
Daniel Mora <daniel_k310a@hotmail.com>
2025-02-24 20:02:43 -0600

    feat: implement product listing and detail screens with navigation updates

--- navigation-app/app/(stack)/_layout.tsx
+++ navigation-app/app/(stack)/_layout.tsx
@@ -1,16 +1,41 @@
-import { Stack } from 'expo-router'
-
+import { Stack } from "expo-router";
 const StackLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        // headerShown: false,
+        headerShadowVisible: false,
+        contentStyle: {
+          backgroundColor: "white",
+        },
+      }}
+    >
       <Stack.Screen
         name="home/index"
         options={{
-          title: 'Home',
+          title: "Inicio",
+        }}
+      />
+      <Stack.Screen
+        name="products/index"
+        options={{
+          title: "Productos",
+        }}
+      />
+
+      <Stack.Screen
+        name="profile/index"
+        options={{
+          title: "Perfil",
+        }}
+      />
+      <Stack.Screen
+        name="settings/index"
+        options={{
+          title: "Ajustes Pantalla",
         }}
       />
     </Stack>
-  )
-}
-
-export default StackLayout
+  );
+};
+export default StackLayout;
--- navigation-app/app/(stack)/products/index.tsx
+++ navigation-app/app/(stack)/products/index.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import { products } from "@/store/products.store";
+import { Link } from "expo-router";
 
-const Products = () => {
+const ProductsScreen = () => {
   return (
-    <View>
-      <Text>Products</Text>
-    </View>
-  )
-}
+    <View className="flex flex-1 px-4">
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View className="mt-10">
+            <Text className="text-2xl font-work-black">{item.title}</Text>
+            <Text className="">{item.description}</Text>
 
-export default Products
+            <View className="flex flex-row justify-between mt-2">
+              <Text className="font-work-black">{item.price}</Text>
+              <Link
+                href={`/(stack)/products/${item.id}`}
+                className="text-primary"
+              >
+                Ver detalles
+              </Link>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+export default ProductsScreen;
